fe6873f5dce6e1a03a83f4f77234817910ac47a9
Shyam Sunder <sundxrr@gmail.com>
2026-08-13 01:17:41 +0530

    feat: implemented serwist service worker to enable native PWA installation

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
+const withSerwist = require("@serwist/next").default({
+  swSrc: "src/app/sw.ts",
+  swDest: "public/sw.js",
+});
+
 const nextConfig: NextConfig = {
   /* config options here */
 };
 
-export default nextConfig;
+export default withSerwist(nextConfig);
